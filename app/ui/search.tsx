7aff2cb8a4f783ee2e8859221806a16c72736a4f
sixthxz <145708241+sixthxz@users.noreply.github.com>
2025-02-26 23:56:51 -0600

    live dj set fixing bugs

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,6 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -12,7 +11,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     
-    const params = new URLSearchParams(searchParams);
+    // Verificamos si searchParams es null antes de usarlo
+    const params = searchParams ? new URLSearchParams(searchParams) : new URLSearchParams();
     params.set('page', '1');
 
     if (term) {
@@ -35,7 +35,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams?.get('query') ?? ''}  // Agregado un fallback por si searchParams es null
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
