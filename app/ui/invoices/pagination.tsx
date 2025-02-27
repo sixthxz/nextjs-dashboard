9a77c6fcba4dd7b6aec495c238c602b085fccf6a
sixthxz <145708241+sixthxz@users.noreply.github.com>
2025-02-26 23:41:01 -0600

    live dj set

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -10,7 +10,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+  const currentPage = Number(searchParams?.get('page')?? 1) || 1;
   
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
