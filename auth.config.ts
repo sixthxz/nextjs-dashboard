ea43a797993718e2995a717b487cc604e82ddf12
sixthxz <145708241+sixthxz@users.noreply.github.com>
2025-02-27 00:37:40 -0600

    live dj set fixing bugs

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,7 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+
 export const authConfig = {
+  secret: process.env.AUTH_SECRET,  
   pages: {
     signIn: '/login',
   },
@@ -10,12 +11,12 @@ export const authConfig = {
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false; // Redirigir usuarios no autenticados al login
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [], // Agrega los proveedores con un array vacío por ahora
 } satisfies NextAuthConfig;
